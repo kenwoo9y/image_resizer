20cf76d429167bc57658e14242276f99b40f7130
kenwoo9y <ei.1px.9.01@gmail.com>
2020-06-20 23:15:52 +0900

    modify fileReader()

--- resize.js
+++ resize.js
@@ -1,3 +1,11 @@
-function fileReader() {}
+function fileReader() {
+    let file = document.querySelector('input[type=file]').files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = function() {
+        let dataUri = this.result;
+        console.log(dataUri);
+    };
+    fileReader.readAsDataURL(file);
+}
 
 function resizeImage(dataUri) {}
