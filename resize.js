1162e95f937ac110bb7795ec15101d2ee7fa5b40
kenwoo9y <ei.1px.9.01@gmail.com>
2020-06-21 18:44:28 +0900

    update resizeImage()

--- resize.js
+++ resize.js
@@ -14,7 +14,14 @@ $('.file-input').on('change',function(){
 
 $('#button').on('click', function(){
     let dataUri = $('#image').attr('src');
-    console.log(dataUri);
+    
+    //画像のリサイズ
+    resizeImage(dataUri).then(function(resizedDataUri) {
+        console.log(resizedDataUri);
+    })
+    .catch(function(error) {
+        alert(error);
+    });
 })
 
 function fileReader(file) {
@@ -34,4 +41,32 @@ function fileReader(file) {
     });
 }
 
-function resizeImage(dataUri) {}
+function resizeImage(dataUri) {
+    return new Promise(function(resolve, reject) {
+        let image = new Image();
+
+        image.onload = function(event) {
+            let ratio = 0.5;
+
+            let canvas = $('#canvas').attr('width', image.width * ratio).attr('height', image.height * ratio);
+            let ctx = canvas[0].getContext('2d');
+
+            //canvasに描画されている画像を削除
+            ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
+
+            //画像を描画する
+            ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width * ratio, image.height * ratio);
+
+            //DataURIを取得
+            let resizedDataUri = canvas[0].toDataURL('image/jpeg');
+
+            resolve(resizedDataUri);
+        };
+
+        image.onerror = function(error) {
+            reject(error);
+        };
+        
+        image.src = dataUri;
+    });
+}
