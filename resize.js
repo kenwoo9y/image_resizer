f1444d0ca7d68558eaa2435892d56bc40c53f3bc
kenwoo9y <ei.1px.9.01@gmail.com>
2020-06-22 23:23:58 +0900

    modify comment

--- resize.js
+++ resize.js
@@ -1,5 +1,5 @@
 $('.file-input').on('change',function(){
-    //ファイルの取得
+    // ファイルの取得
     let file = $(this).prop('files')[0];
 
     // ファイルの読み込み
@@ -51,13 +51,13 @@ function resizeImage(dataUri) {
             let canvas = $('#canvas').attr('width', image.width * ratio).attr('height', image.height * ratio);
             let ctx = canvas[0].getContext('2d');
 
-            //canvasに描画されている画像を削除
+            // canvasに描画されている画像を削除
             ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
 
-            //画像を描画する
+            // 画像を描画する
             ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width * ratio, image.height * ratio);
 
-            //DataURIを取得
+            // DataURIを取得
             let resizedDataUri = canvas[0].toDataURL('image/jpeg');
 
             resolve(resizedDataUri);
